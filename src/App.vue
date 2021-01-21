f7e85725f9f6eaac2920b4493b155bec8b2e4161
KaYo <kayoczl@163.com>
2021-01-21 23:59:35 +0800

    5-1Nav-Header

--- src/App.vue
+++ src/App.vue
@@ -13,33 +13,15 @@ export default {
   },
   data(){
     return {
-      res:{}
+
     }
   },
   mounted(){
-    // 本地加载请求静态json文件的形式
-    // this.axios.get('/mock/user/login.json').then((res) => {
-    //   this.res = res;
-    // });
-    // 通过easy-mock平台实现数据mock
-    // this.axios.get('/user/login').then((res) => {
-    //   this.res = res;
-    // });
-    // 本地集成mockjs实现数据mock
-    this.axios.get('/user/login').then((res) => {
-      this.res = res;
-    });
+    
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+@import './assets/scss/reset.scss'
 </style>
