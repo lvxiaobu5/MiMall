109dfdff2915ce331ff1c36b5424c93bbeb04b3c
KaYo <kayoczl@163.com>
2021-01-31 22:25:58 +0800

    5-11首页广告位实现

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -146,8 +146,16 @@
           <div class="swiper-button-next" slot="button-next"></div>
         </swiper>
       </div>
-      <div class="ads-box"></div>
-      <div class="banner"></div>
+      <div class="ads-box">
+        <a :href="'/#/product/'+item.id" v-for="(item,index) in adsList" :key="index">
+          <img :src="item.img" alt="">
+        </a>
+      </div>
+      <div class="banner">
+        <a href="/#/product/30">
+          <img src="/imgs/banner-1.png" alt="">
+        </a>
+      </div>
       <div class="product-box"></div>
     </div>
     <service-bar></service-bar>
@@ -223,6 +231,21 @@ export default {
           }
         ],
         [0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0]
+      ],
+      adsList:[
+        {
+          id:33,
+          img:'/imgs/ads/ads-1.png'
+        }, {
+          id:48,
+          img:'/imgs/ads/ads-2.jpg'
+        }, {
+          id:45,
+          img:'/imgs/ads/ads-3.png'
+        }, {
+          id:47,
+          img:'/imgs/ads/ads-4.jpg'
+        }, 
       ]
     }
   },
@@ -230,7 +253,7 @@ export default {
 </script>
 
 <style lang="scss">
-@import './../assets/scss/config.scss';
+  @import './../assets/scss/config.scss';
   @import './../assets/scss/mixin.scss';
   .index{
     .swiper-box{
@@ -311,6 +334,18 @@ export default {
           height: 100%;
         }
       }
+    }
+    .ads-box{
+      @include flex();
+      margin-top: 14px;
+      margin-bottom: 31px;
+      a{
+        width: 296px;
+        height: 167px;
+      }
+    }
+    .banner{
+      margin-bottom: 50px;
     }
   }
 </style>
