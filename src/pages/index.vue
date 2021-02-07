39937b19c6fa966a8868dadc7d18d0df395dc919
KaYo <kayoczl@163.com>
2021-02-08 02:41:01 +0800

    5-16Modal动画实现

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -174,7 +174,7 @@
                 <div class="item-info">
                   <h3>{{item.name}}</h3>
                   <p>{{item.subtitle}}</p>
-                  <p class="price">{{item.price}}元</p>
+                  <p class="price" @click="addCart(item.id)">{{item.price}}元</p>
                 </div>
               </div>
             </div>
@@ -188,7 +188,9 @@
       sureText="查看购物车" 
       btnType="1" 
       modalType="middle"
-      :showModal="true">
+      :showModal="showModal"
+      @submit="goToCart"
+      @cancel="showModal=false">
       <template v-slot:body>
         <p>商品添加成功！</p>
       </template>
@@ -283,7 +285,8 @@ export default {
           img:'/imgs/ads/ads-4.jpg'
         }, 
       ],
-      phoneList:[]
+      phoneList:[],
+      showModal:false,
     }
   },
   mounted() {
@@ -300,6 +303,20 @@ export default {
         res.list = res.list.slice(6, 14);
         this.phoneList = [res.list.slice(0,4),res.list.slice(4,8)];
       })
+    },
+    addCart(){
+      this.showModal = true;
+      // this.axios.post('/carts',{
+      //   productId:id,
+      //   selected:true
+      // }).then(() => {
+
+      // }).catch(() => {
+      //   this.showModal = true;
+      // })
+    },
+    goToCart(){
+      this.$router.push('/cart');
     }
   },
 }
