78baea72b96307c48766db9f077a67c0d207d4a9
KaYo <kayoczl@163.com>
2021-02-08 21:05:45 +0800

    5-17Modal交互补充

--- src/components/Modal.vue
+++ src/components/Modal.vue
@@ -4,18 +4,18 @@
       <div class="mask"></div>
       <div class="modal-dialog">
         <div class="modal-header">
-          <span>标题</span>
+          <span>{{title}}</span>
           <a href="javascript:;" class="icon-close" @click="$emit('cancel')"></a>
         </div>
         <div class="modal-body">
           <slot name="body"></slot>
         </div>
         <div class="modal-footer">
-          <a href="javascript:;" class="btn" v-if="btnType==1" @click="$emit('submit')">确定</a>
-          <a href="javascript:;" class="btn" v-if="btnType==2" @click="$emit('cancel')">取消</a>
+          <a href="javascript:;" class="btn" v-if="btnType==1" @click="$emit('submit')">{{sureText}}</a>
+          <a href="javascript:;" class="btn" v-if="btnType==2" @click="$emit('cancel')">{{cancelText}}</a>
           <div class="btn-group" v-if="btnType==3">
-            <a href="javascript:;" class="btn" @click="$emit('submit')">确定</a>
-            <a href="javascript:;" class="btn" @click="$emit('cancel')">取消</a>
+            <a href="javascript:;" class="btn" @click="$emit('submit')">{{sureText}}</a>
+            <a href="javascript:;" class="btn" @click="$emit('cancel')">{{cancelText}}</a>
           </div>
         </div>
       </div>
