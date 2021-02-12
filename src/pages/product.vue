30c5a8024baad982e6b50d92dfb4582e3c833153
KaYo <kayoczl@163.com>
2021-02-13 01:42:48 +0800

    8-1产品站参数组件实现

--- src/pages/product.vue
+++ src/pages/product.vue
@@ -1,14 +1,26 @@
 <template>
-  <div>
-    product
+  <div class="product">
+    <product-param>
+      <template v-slot:buy>
+        <button class="btn">立即购买</button>
+      </template>
+    </product-param>
   </div>
 </template>
 
 <script>
+import ProductParam from './../components/ProductParam'
 export default {
   components: {
-    
+    ProductParam
   },
   name:'product'
 }
 </script>
+<style lang="scss">
+  .product{
+    button{
+      margin-left: 10px;
+    }
+  }
+</style>
