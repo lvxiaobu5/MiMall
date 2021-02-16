2cd03ea40a40cd3490cd591ea4f881988e456b04
KaYo <kayoczl@163.com>
2021-02-16 19:04:02 +0800

    10-3购物车全选和非全选

--- src/pages/cart.vue
+++ src/pages/cart.vue
@@ -9,7 +9,7 @@
       <div class="container">
         <div class="cart-box">
           <ul class="cart-item-head">
-            <li class="col-1"><span class="checkbox" v-bind:class="{'checked':allChecked}" @click="toggleAll"></span>全选</li>
+            <li class="col-1"><span class="checkbox" :class="{'checked':allChecked}" @click="toggleAll"></span>全选</li>
             <li class="col-3">商品名称</li>
             <li class="col-1">单价</li>
             <li class="col-2">数量</li>
@@ -90,6 +90,12 @@
         this.cartTotalPrice = res.cartTotalPrice;
         this.checkedNum = this.list.filter(item=>item.productSelected).length;
       },
+      toggleAll(){
+        let url = this.allChecked ? '/carts/unSelectAll' : '/carts/selectAll';
+        this.axios.put(url).then((res)=>{
+          this.renderData(res);
+        })
+      }
     }
   }
 </script>
