279f1e0d38fe357f35589b73e2da9caf49470709
KaYo <kayoczl@163.com>
2021-02-20 02:21:57 +0800

    12-5新增地址交互实现

--- src/pages/orderConfirm.vue
+++ src/pages/orderConfirm.vue
@@ -114,21 +114,21 @@
       <template v-slot:body>
         <div class="edit-wrap">
           <div class="item">
-            <input type="text" class="input" placeholder="姓名">
-            <input type="text" class="input" placeholder="手机号">
+            <input type="text" class="input" placeholder="姓名" v-model="checkdeItem.receiverName">
+            <input type="text" class="input" placeholder="手机号" v-model="checkdeItem.receiverMobile">
           </div>
           <div class="item">
-            <select name="province">
+            <select name="province" v-model="checkdeItem.receiverProvince">
               <option value="北京">北京</option>
               <option value="上海">上海</option>
               <option value="广东">广东</option>
             </select>
-            <select name="city">
+            <select name="city" v-model="checkdeItem.receiverCity">
               <option value="北京">北京</option>
               <option value="上海">上海</option>
               <option value="广东">广州</option>
             </select>
-            <select name="district">
+            <select name="district" v-model="checkdeItem.receiverDistrict">
               <option value="北京">昌平区</option>
               <option value="上海">浦东区</option>
               <option value="广东">天河区</option>
@@ -138,10 +138,10 @@
             </select>
           </div>
           <div class="item">
-            <textarea name="street"></textarea>
+            <textarea name="street" v-model="checkdeItem.receiverAddress"></textarea>
           </div>
           <div class="item">
-            <input type="text" class="input" placeholder="邮编">
+            <input type="text" class="input" placeholder="邮编" v-model="checkdeItem.receiverZip">
           </div>
         </div>
       </template>
@@ -189,6 +189,12 @@ export default {
         this.list = res.list;
       })
     },
+    // 打开新增地址弹框
+    openAddressModal(){
+      this.userAction = 0;
+      this.checkdeItem = {};
+      this.showEditModal = true;
+    },
     delAddress(item){
       this.checkdeItem = item;
       this.userAction = 2;
@@ -197,7 +203,7 @@ export default {
     // 地址删除、编辑、新增功能
     submitAddress(){
       let { checkedItem,userAction } = this;
-      let method,url;
+      let method,url,params={};
       if (userAction == 0) {
         method = 'post',url = '/shippings';
       } else if (userAction == 1) {
@@ -205,7 +211,37 @@ export default {
       } else {
         method = 'delete',url = `/shippings/${checkedItem.id}`;
       }
-      this.axios[method](url).then(()=>{
+      if (userAction == 0 || userAction == 1) {
+        let { receiverName, receiverMobile, receiverProvince, receiverCity, receiverDistrict, receiverAddress, receiverZip} = checkedItem;
+        let errMsg;
+        if (!receiverName) {
+          errMsg = '请输入收货人名字';
+        } else if (!receiverMobile || !/\d{11}/.test(receiverMobile)) {
+          errMsg = '请输入正确格式的手机号';
+        } else if (!receiverProvince){
+          errMsg = '请选择省份';
+        } else if (!receiverCity){
+          errMsg = '请选择对应的城市';
+        } else if (!receiverAddress || !receiverDistrict){
+          errMsg = '请输入收货地址';
+        } else if (!/\d{6}/.test(receiverZip)){
+          errMsg = '请输入六位邮编';
+        }
+        if (errMsg) {
+          this.$message.error(errMsg);
+          return;
+        }
+        params = {
+          receiverName,
+          receiverMobile,
+          receiverProvince,
+          receiverCity,
+          receiverDistrict,
+          receiverAddress,
+          receiverZip
+        }
+      }
+      this.axios[method](url,params).then(()=>{
         this.closeModal();
         this.getAddressList();
         this.$message.success('操作成功');
@@ -215,6 +251,7 @@ export default {
       this.checkdeItem = {};
       this.userAction = '';
       this.showDelModal = false;
+      this.showEditModal = false;
     },
     getCartList(){
       this.axios.get('/carts').then((res)=>{
