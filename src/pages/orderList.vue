62849c048f1650988e9eced8680697b0ad18980e
KaYo <kayoczl@163.com>
2021-02-28 22:59:03 +0800

    14-4订单列表分页-分页器

--- src/pages/orderList.vue
+++ src/pages/orderList.vue
@@ -46,6 +46,14 @@
               </div>
             </div>
           </div>
+          <el-pagination
+            class="pagination"
+            background
+            layout="prev, pager, next"
+            :pageSize="pageSize"
+            :total="total"
+            @current-change="handleChange">
+          </el-pagination>
           <no-data v-if="!loading && list.length==0"></no-data>
         </div>
       </div>
@@ -56,17 +64,22 @@
   import OrderHeader from './../components/OrderHeader'
   import Loading from './../components/Loading'
   import NoData from './../components/NoData'
+  import { Pagination } from 'element-ui'
   export default{
     name:'order-list',
     components:{
       OrderHeader,
       Loading,
-      NoData
+      NoData,
+      [Pagination.name]:Pagination,
     },
     data() {
       return {
         list:[],    //订单列表
         loading:true,//默认显示loading
+        pageSize:10,
+        pageNum:1,
+        total:0,
       }
     },  
     mounted() {
@@ -74,9 +87,14 @@
     },
     methods:{
       getOrderList(){
-        this.axios.get('/orders').then((res)=>{
+        this.axios.get('/orders',{
+          params:{
+            pageNum: this.pageNum,
+          }
+        }).then((res)=>{
           this.loading = false;
           this.list = res.list;
+          this.total = res.total;
         }).catch(()=>{
           this.loading = false;
         })
@@ -97,7 +115,10 @@
             orderNo
           }
         })
-        
+      },
+      handleChange(pageNum){
+        this.pageNum = pageNum;
+        this.getOrderList();
       }
     }
   }
@@ -149,8 +170,9 @@
                   }
                 }
                 .good-name{
-                  font-size:20px;
+                  font-size:18px;
                   color:$colorB;
+                  margin-left: 10px;
                 }
               }
             }
