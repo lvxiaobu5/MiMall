a46669309eb9089ffe83a4f0f3c80e76737634d5
KaYo <kayoczl@163.com>
2021-03-02 02:58:01 +0800

    15-2vue.config配置介绍

--- vue.config.js
+++ vue.config.js
@@ -11,5 +11,11 @@ module.exports = {
         }
       }
     }
-  }
+  },
+  // publicPath:'/app',//在每个页面的路径前加上/app
+  // outputDir:'dist', //输出项目的目录
+  // indexPath:'index2.html',//指定输出的根路径这个单页面的名字叫什么
+  // lintOnSave:false,//ESLint开关
+  productionSourceMap:false,//加速生产环境构建，删除.map文件
+
 }
